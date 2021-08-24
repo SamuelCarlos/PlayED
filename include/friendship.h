64d9587fc6f53b3ef0a7501ea1bb762a7bb25e47
Bruno Carneiro Izoton <bruno@DESKTOP-T9P71DS.localdomain>
2021-08-23 23:44:59 -0300

    friendship.h song_list.h and song.h comments

--- include/friendship.h
+++ include/friendship.h
@@ -7,14 +7,39 @@
 
 typedef struct Friendship Friendship;
 
+/**
+ * @brief initializes and allocates space for friends list;
+ * @param newFriendship: a friends list ;
+ * @return the allocated list ;
+ */
 Friendship* initializeFriendship(Friendship* newFriendship);
 
+/**
+ * @brief creates a new cell with the new friend and insert in frendship list;
+ * @param friendName: the name of the new friend;
+ * @return the list with the new cell ;
+ */
 Friendship* insertFriendship(Friendship* list, char* friendName);
 
+/**
+ * @brief free the friendship list;
+ * @param list: a friendship list ;
+ */
 void freeFriendship(Friendship* list);
 
+/**
+ * @brief print the friendship list;
+ * @param list: a friendship list ;
+ */
 void printFriendship(Friendship* friendship);
 
+/**
+ * @brief receives the name of a user and searches a list of friends if that user is on the list;
+ * @param friendship: a friendship list ;
+ * @param friend: the name of the friend who is being looked for;
+ * @return 1: if they are friends ;
+ * @return 0: if not friends;
+ */
 int friendIterator(Friendship* friendship, char *friend);
 
 #endif //FRIENDSHIP_H
--- include/song.h
+++ include/song.h
@@ -7,16 +7,45 @@
 
 typedef struct Song Song;
 
+/**
+ * @brief receives the song data and create a new song;
+ * @param name: the song name;
+ * @param artist: the artist;
+ * @return the new song;
+ */
 Song* createSong(char* name, char* artist);
 
+/**
+ * @brief free the song struct pointer;
+ * @param song: a song struct pointer;
+ */
 void freeSong(Song* song);
 
+/**
+ * @brief print the song data;
+ * @param song: a song struct pointer;
+ */
 void printSong(Song* song);
 
+/**
+ * @brief receives a song and get the artist name;
+ * @param song: a song struct pointer;
+ * @return string with the artist name;
+ */
 char *getArtistName(Song *song);
 
+/**
+ * @brief receives a song and get the song name;
+ * @param song: a song struct pointer;
+ * @return string with the song name;
+ */
 char *getSongName(Song *song);
 
+/**
+ * @brief print the song data in a file;
+ * @param song: a song struct pointer;
+ * @param file: the destination file;
+ */
 void printSonginFile(Song* song,FILE* file);
 
 #endif //SONG_H
